af5e6d906344a84e0ac42cce02cd5f9b285d0de3
MOHAN KUMAR A <AM9549@amnet.in>
2021-12-19 15:20:58 +0530

    replaced React folder to react

---
+++ react/robofriends/src/containers/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchFields: ""
+        };
+        // this.onSearchChange = this.onSearchChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((robots) => this.setState({robots}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchFields: event.target.value})
+    }
+
+    render() {
+        const { robots, searchFields } = this.state;
+        const filterRobots = robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(searchFields.toLowerCase())
+        });
+        return (!robots.length)
+            ?
+            (<h1>Loading...</h1>)
+            :
+            (
+                <div className="tc">
+                    <h1 className="f1">Robot Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filterRobots} />
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            )
+    }
+}
+
+export default App;
